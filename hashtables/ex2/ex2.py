1f9bdf08100bb6239aaa8786b536401fcfa60ed2
Sandy <sandy.jernigan@gmail.com>
2020-04-19 21:30:32 -0400

    HashTable Ex2

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -17,7 +17,33 @@ def reconstruct_trip(tickets, length):
     route = [None] * length
 
     """
-    YOUR CODE HERE
+    reconstruct your trip from your mass of flight tickets
+    source string represents the starting airport and
+    destination string represents the next airport along our trip
+    The ticket for your first flight has a destination with a source of NONE,
+    and the ticket for your final flight has a source with a destination of NONE.
     """
 
-    pass
+    return None
+
+
+# Test function
+ticket_1 = Ticket("PIT", "ORD")
+ticket_2 = Ticket("XNA", "SAP")
+ticket_3 = Ticket("SFO", "BHM")
+ticket_4 = Ticket("FLG", "XNA")
+ticket_5 = Ticket("NONE", "LAX")
+ticket_6 = Ticket("LAX", "SFO")
+ticket_7 = Ticket("SAP", "SLC")
+ticket_8 = Ticket("ORD", "NONE")
+ticket_9 = Ticket("SLC", "PIT")
+ticket_10 = Ticket("BHM", "FLG")
+
+tickets = [ticket_1, ticket_2, ticket_3, ticket_4, ticket_5,
+            ticket_6, ticket_7, ticket_8, ticket_9, ticket_10]
+
+expected = ["LAX", "SFO", "BHM", "FLG", "XNA", "SAP",
+            "SLC", "PIT", "ORD"]
+
+result = reconstruct_trip(tickets, 10)
+print(result)
